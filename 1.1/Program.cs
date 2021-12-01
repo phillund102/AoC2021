fd7d14261c759051d15f106d935bff4a1df543da
Philip <Philip@DESKTOP-MJDG8J9>
2021-12-01 06:21:56 +0100

    Add project files.

---
+++ 1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int input = Int32.Parse(Console.ReadLine());
+int input2;
+int counter = 0;
+
+while (input != 0)
+{
+    input2 = Int32.Parse(Console.ReadLine());
+    if (input2 > input)
+    {
+        counter++;
+    }
+    input = input2;
+}
+
+Console.WriteLine(counter); 
---
+++ 1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<int> theList = new List<int>();
+bool loop = true;
+int sum = 0, counter = 0;
+int input = Int32.Parse(Console.ReadLine());
+int input2 = Int32.Parse(Console.ReadLine());
+
+while (loop)
+{
+    int input3 = Int32.Parse(Console.ReadLine());
+    if(input3 == 0)
+    {
+        break;
+    }
+    sum = input + input2 + input3;
+    theList.Add(sum);
+
+    input = input2;
+    input2 = input3;
+
+
+}
+
+for(int i = 0; i < theList.Count; i++)
+{
+    if(i > 0)
+    {
+        if (theList[i] > theList[i - 1])
+        {
+            counter++;
+        }
+    }
+}
+
+Console.WriteLine(counter);
